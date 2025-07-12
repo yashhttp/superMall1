ba73ab3bd44b0c6f5f68c3a2a407940bcd29cecd
Yash Saini <yashsainishamli12@gmail.com>
2025-07-12 18:31:15 +0530

    Update Navbar.js
    category name error solve

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,76 +1,4 @@
-// import React, { useState, useEffect} from 'react';
-// import "./Navbar.scss";
-// import { Link } from 'react-router-dom';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchCategories } from '../../store/categorySlice';
-// import { getCartTotal } from '../../store/cartSlice';
 
-// const Navbar = () => {
-//   const dispatch = useDispatch();
-//   const {data: categories} = useSelector((state) => state.category);
-//   const {totalItems} = useSelector((state => state.cart));
-
-//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-//   useEffect(() => {
-//     dispatch(fetchCategories());
-//     dispatch(getCartTotal());
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   return (
-//     <nav className = "navbar">
-//       <div className='navbar-content'>
-//         <div className = "container">
-//           <div className = "navbar-top flex flex-between">
-//               <Link to = "/" className = "navbar-brand">
-//                 <span className = "text-regal-blue">Shopping</span><span className='text-gold'>Hub.</span>
-//               </Link>
-
-//               <form className = "navbar-search flex">
-//                 <input type = "text" placeholder='Search here ...' />
-//                 <button type = "submit" className = "navbar-search-btn">
-//                   <i className = "fas fa-search"></i>
-//                 </button>
-//               </form>
-
-//               <div className = "navbar-btns">
-//                 <Link to = "/cart" className="add-to-cart-btn flex">
-//                   <span className = "btn-ico">
-//                     <i className = "fas fa-shopping-cart"></i>
-//                   </span>
-//                   <div className='btn-txt fw-5'>Cart
-//                     <span className='cart-count-value'>{totalItems}</span>
-//                   </div>
-//                 </Link>
-//               </div>
-//           </div>
-//         </div>
-        
-//         <div className='navbar-bottom bg-regal-blue'>
-//           <div className='container flex flex-between'>
-//             <ul className = {`nav-links flex ${isSidebarOpen ? 'show-nav-links' : ""}`}>
-//               <button type = "button" className='navbar-hide-btn text-white' onClick={() => setIsSidebarOpen(false)}>
-//                 <i className='fas fa-times'></i>
-//               </button>
-//               {
-//                 categories.map(category => (
-//                   <li key = {category.id}><Link to = {`/category/${category.id}`} className = "nav-link text-white" onClick={() => setIsSidebarOpen(false)}>{category.name}</Link></li>
-//                 ))
-//               }
-//             </ul>
-
-//             <button type = "button" className='navbar-show-btn text-gold' onClick={() => setIsSidebarOpen(true)}>
-//               <i className = "fas fa-bars"></i>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   )
-// }
-
-// export default Navbar;
 import React, { useState, useEffect } from 'react';
 import "./Navbar.scss";
 import { Link } from 'react-router-dom';
@@ -110,7 +38,7 @@ const Navbar = () => {
 
             <div className="navbar-btns flex">
 
-              {/* 🛒 Cart Button */}
+
               <Link to="/cart" className="add-to-cart-btn flex">
                 <span className="btn-ico">
                   <i className="fas fa-shopping-cart"></i>
@@ -120,10 +48,9 @@ const Navbar = () => {
                 </div>
               </Link>
 
-              {/* 🔐 Login Button */}
               <Link to="/login" className="btn btn-secondary mx-2">Login</Link>
 
-              {/* 📝 Signup Button */}
+
               <Link to="/signup" className="btn btn-gold">Signup</Link>
 
             </div>
@@ -140,7 +67,8 @@ const Navbar = () => {
                 categories.map(category => (
                   <li key={category.id}>
                     <Link to={`/category/${category.id}`} className="nav-link text-white" onClick={() => setIsSidebarOpen(false)}>
-                      {category.name}
+                      // {category.name}
+                       {category.name === 'nik' ? 'cloth' : category.name}
                     </Link>
                   </li>
                 ))
